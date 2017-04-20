25fbcc2d79165fdaa8f96359c8aa2ea68e5c2bce
Daniele D'Orazio <d.dorazio96@gmail.com>
2017-04-20 22:17:03 +0200

    pretty print

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 
+import Lib
 import Parser
 
 main :: IO ()
-main = mapM_ (\l -> putStr l >> putStr " -> " >> parseTest line l) input
+main = mapM_ go input
   where
+    go l = do
+      putStr l
+      putStr " -> "
+      let (Right a) = parse line "<test>" l
+      putStr . show $ a
+      putStr " -> "
+      putStrLn . render . pPrint $ a
     input = ["x", "λx.x", "x y", "(λx y.x) a b"]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,12 @@
 module Lib
   ( AST(..)
   , lam
+  , pPrint
+  , render
   ) where
+
+import Text.PrettyPrint.HughesPJ
+import Text.PrettyPrint.HughesPJClass (Pretty(..))
 
 lam :: Char
 lam = '\955' -- aka 0x03BB
@@ -13,3 +18,12 @@ data AST
   | App AST
         AST
   deriving (Show, Eq)
+
+instance Pretty AST where
+  pPrint (Var v) = text v
+  pPrint (Abs arg body) = char lam <> text arg <> char '.' <> pPrint body
+  pPrint (App fn arg) = maybeParens (not $ isVar fn) (pPrint fn) <+> pPrint arg
+
+isVar :: AST -> Bool
+isVar (Var _) = True
+isVar _ = False
