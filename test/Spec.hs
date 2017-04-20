3170734e736246545aaa3847b1583196dcef9ea3
Daniele D'Orazio <d.dorazio96@gmail.com>
2017-04-20 23:06:28 +0200

    quickcheck

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,27 @@
+import Lib
+import Parser
+
+import Test.QuickCheck
+
+instance Arbitrary AST where
+  arbitrary = do
+    n <- choose (0, 2) :: Gen Integer
+    case n of
+      0 -> fmap Var string
+      1 -> Abs <$> string <*> arbitrary
+      2 -> App <$> arbitrary <*> arbitrary
+      _ -> error "should be impossible"
+
+string :: Gen String
+string = listOf1 char
+
+char :: Gen Char
+char = elements ['a' .. 'z']
+
+prop_parsePrettyPrint :: AST -> Bool
+prop_parsePrettyPrint a =
+  let (Right r) = parse line "<test>" (render . pPrint $ a)
+  in r == a
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = quickCheck prop_parsePrettyPrint
