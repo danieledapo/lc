f87fabb542dd045cf01969fa2eadfe85ebec8c21
Daniele D'Orazio <d.dorazio96@gmail.com>
2017-05-22 22:19:21 +0200

    [wip] naive

--- src/Language/Lc.hs
+++ src/Language/Lc.hs
@@ -1,10 +1,16 @@
 -- | A simple lambda calculus
 module Language.Lc
   ( Lc(..)
+  , naiveBetaReduce
+  , naiveBetaReduceAll
+  , naiveEval
   , lam
   , pPrint
   , render
   ) where
+
+import Data.Maybe (fromMaybe)
+import qualified Data.Map as Map
 
 import Text.PrettyPrint.HughesPJ
 import Text.PrettyPrint.HughesPJClass (Pretty(..))
@@ -31,3 +37,52 @@ instance Pretty Lc where
 isVar :: Lc -> Bool
 isVar (LcVar _) = True
 isVar _ = False
+
+
+--------------------------------------------------------------
+-- Naive interpreter
+--------------------------------------------------------------
+
+-- | the environment a LcAbs is evaluated in
+type Environment = Map.Map String Lc
+
+-- | evaluate the given Lc, returns the given one if it's
+-- not reducible
+naiveEval :: Lc -> Lc
+naiveEval lc = case naiveBetaReduceAll lc of
+  [] -> lc
+  bs -> last bs
+
+-- | betaReduce the given Lc until no further reductions
+-- are possible
+naiveBetaReduceAll :: Lc -> [Lc]
+naiveBetaReduceAll lc =
+  let lc' = naiveBetaReduce lc
+  in if lc /= lc'
+      then lc' : naiveBetaReduceAll lc'
+      else []
+
+-- | naive betaReduce
+naiveBetaReduce :: Lc -> Lc
+naiveBetaReduce = betaReduceWithEnv Map.empty
+
+
+-- | betaReduce in the given environment
+betaReduceWithEnv :: Environment -> Lc -> Lc
+betaReduceWithEnv env lcV@(LcVar v) =
+  fromMaybe lcV $ Map.lookup v env
+
+betaReduceWithEnv env' (LcAbs p body) =
+  let env'' = Map.delete p env'
+  in LcAbs p $ betaReduceWithEnv env'' body
+
+betaReduceWithEnv env' (LcApp (LcAbs p body) arg) =
+  let env'' = Map.insert p arg env'
+  in betaReduceWithEnv env'' body
+
+betaReduceWithEnv env' app@(LcApp fn arg) =
+  let fn' = betaReduceWithEnv env' fn
+      arg' = betaReduceWithEnv env' arg
+  in if fn /= fn' || arg /= arg'
+      then LcApp fn' arg'
+      else app
