244233987ceaa57be555170744761a21a8133702
d-dorazio <d.dorazio96@gmail.com>
2017-08-31 12:29:43 +0200

    format

--- src/Language/Lc/Interpreter/Dynamic.hs
+++ src/Language/Lc/Interpreter/Dynamic.hs
@@ -32,23 +32,19 @@ instance Interpreter DynamicInterpreter where
 dynamicBetaReduce :: Lc -> Lc
 dynamicBetaReduce (LcApp (LcAbs p fn) arg) = substitute fn p arg
 dynamicBetaReduce mainApp@(LcApp fn arg) =
-  let fn' = dynamicBetaReduce fn
+  let fn'  = dynamicBetaReduce fn
       arg' = dynamicBetaReduce arg
-  in if fn /= fn' || arg /= arg' -- ensure both fn and arg have already been reduced
-       then LcApp fn' arg'
-       else mainApp
+  in  if fn /= fn' || arg /= arg' -- ensure both fn and arg have already been reduced
+        then LcApp fn' arg'
+        else mainApp
 dynamicBetaReduce lc = lc
 
 -- | substitute in the given 'Lc' the LcVars with the given name
 -- with the given 'Lc'
 substitute :: Lc -> String -> Lc -> Lc
-substitute v@(LcVar var) param new =
-  if var == param
-    then new
-    else v
-substitute lcAbs@(LcAbs newParam fn) oldParam new =
-  if newParam /= oldParam
-    then LcAbs newParam $ substitute fn oldParam new
-    else lcAbs -- oldParam has been shadowed in this case
-substitute (LcApp fn arg) param new =
-  LcApp (substitute fn param new) (substitute arg param new)
+substitute v@(LcVar var) param new | var == param = new
+                                   | otherwise    = v
+substitute lcAbs@(LcAbs newParam fn) oldParam new
+  | newParam /= oldParam = LcAbs newParam $ substitute fn oldParam new
+  | otherwise            = lcAbs -- oldParam has been shadowed in this case
+substitute (LcApp fn arg) param new = LcApp (substitute fn param new) (substitute arg param new)
